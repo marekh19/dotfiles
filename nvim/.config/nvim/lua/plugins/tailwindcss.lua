41fe835db1a6594e24b09cc52964912979aede67
Marek Honzal <79986994+marekh19@users.noreply.github.com>
2023-07-08 21:56:05 +0200

    feat: add tailwind colorizer

---
+++ nvim/.config/nvim/lua/plugins/tailwindcss.lua
@@ -0,0 +1,11 @@
+-- Add color bg to tailwindcss classes in the editor
+return {
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      user_default_options = {
+        tailwind = true,
+      },
+    },
+  },
+}
