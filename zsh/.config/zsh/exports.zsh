964414e44ef052a0408b8b5c21ca3a1766f6777a
Marek Honzal <79986994+marekh19@users.noreply.github.com>
2023-07-22 10:40:35 +0200

    feat: add global npm support

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -1,6 +1,13 @@
 #!/bin/sh
 HISTSIZE=1000000
 SAVEHIST=1000000
+
+# Global npm installs without sudo
+NPM_PACKAGES="${HOME}/.npm-packages"
+export PATH="$PATH:$NPM_PACKAGES/bin"
+export MANPATH="${MANPATH-$(manpath)}:${NPM_PACKAGES}/share/man"
+
 export EDITOR="nvim"
 export TERMINAL="kitty"
+export BROWSER="firefox"
 export XDG_CURRENT_DESKTOP="Wayland"
