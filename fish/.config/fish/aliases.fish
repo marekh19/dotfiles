4e589347693768ee77f3e1ef8575f5a9af7c4e05
Marek Honzal <marek.honzal@gmail.com>
2024-02-11 12:59:16 +0100

    update fish & add neofetch

---
+++ fish/.config/fish/aliases.fish
@@ -0,0 +1,19 @@
+# Basic
+alias ls lsd
+alias ll 'ls -l'
+alias la 'ls -a'
+alias lla 'ls -la'
+alias lt 'ls -a --tree --ignore-glob .git --ignore-glob node_modules --ignore-glob .next'
+
+# Git
+alias ga 'git add .'
+alias gap 'git add -p'
+alias gc 'git commit -m'
+alias gs 'git status'
+alias gp 'git push'
+alias gfp 'git fetch --all && git pull'
+alias lg lazygit
+
+# Utils
+alias vim nvim
+alias ssh 'TERM=xterm-color ssh'
---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+source $HOME/.config/fish/aliases.fish
+
+set -g fish_greeting
+
+set -x N_PREFIX "$HOME/n"
+contains "$N_PREFIX/bin" $PATH; or set -a PATH "$N_PREFIX/bin" # Added by n-install (see http://git.io/n-install-repo).
+
+starship init fish | source
+
+neofetch
