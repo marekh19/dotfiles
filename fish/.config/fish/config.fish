2ec83b6ab06a3a19403e57d4b64fffaeb61d5304
Marek Honzal <marek.honzal@gmail.com>
2024-02-12 09:48:04 +0100

    add path macos

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,6 +10,9 @@ set -g fish_greeting
 set -x N_PREFIX "$HOME/n"
 contains "$N_PREFIX/bin" $PATH; or set -a PATH "$N_PREFIX/bin" # Added by n-install (see http://git.io/n-install-repo).
 
+set PATH $PATH opt/homebrew/bin
+set PATH $PATH opt/homebrew/sbin
+
 starship init fish | source
 
 neofetch
