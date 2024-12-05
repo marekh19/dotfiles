2a1497ec5c6e2050c5e68e064077e3ab7cc6973d
Marek Honzal <marek.honzal@gmail.com>
2024-12-05 17:45:36 +0100

    chore(nvim): update lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -3,8 +3,6 @@ return {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     opts = function(_, opts)
-      opts.options.component_separators = ""
-      opts.options.section_separators = { left = "", right = "" }
       -- disable section "z" section (clock)
       opts.sections.lualine_z = {}
     end,
